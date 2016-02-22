f1198d1292e9accc2c2922168050e06a178c9949
hamima <hhalim1@jhu.edu>
2016-02-22 13:02:58 -0500

    java stuff

--- app/src/main/java/com/example/mobilecomputingprogram/piggydank/MainActivity.java
+++ app/src/main/java/com/example/mobilecomputingprogram/piggydank/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.mobilecomputingprogram.piggydank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private double myCash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goButton = (Button) findViewById(R.id.go_btn);
+        goButton.setOnClickListener(begin);
     }
+
+    private final View.OnClickListener begin = new View.OnClickListener() {
+        public void onClick(View v) {
+            //go to transition activity
+        }
+    };
 }
